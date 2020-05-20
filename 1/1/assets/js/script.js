2d5728744ef4a1a3091ffb557f1b6a7e85f4622b
Mustafa Dalga <mustafadalgaa@gmail.com>
2020-05-21 00:07:46 +0300

    1. Introduction

---
+++ 1/1/assets/js/script.js
@@ -0,0 +1,11 @@
+let canvas = document.getElementById('hello-world-canvas')
+let context = canvas.getContext('2d')
+
+
+// Blue reactangle
+context.fillStyle = "blue";
+context.fillRect(10, 40, 30, 70);
+
+// Yellow reactangle
+context.fillStyle = "yellow"
+context.fillRect(50, 30, 60, 60); //Dikdörtgen
